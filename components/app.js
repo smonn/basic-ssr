af3d1660aee9a6c636ddb526e8b8297777c8aaa7
Simon Ingeson <44818+smonn@users.noreply.github.com>
2020-12-20 22:57:15 -0500

    create new app component

---
+++ components/app.js
@@ -0,0 +1,17 @@
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages";
+import NotFound from "../pages/404";
+import About from "../pages/About";
+
+export default function App() {
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Index />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </Switch>
+  );
+}
